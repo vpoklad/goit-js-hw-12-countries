483a4c201c604650fa4e2da6ef28a4f91637ec6f
Viktor Poklad <3d.poklad@gmail.com>
2021-08-17 00:50:34 +0300

    добавляет разметку и запрос

--- src/index.js
+++ src/index.js
@@ -1,1 +1,41 @@
 import './sass/main.scss';
+import listTemplate from './templates/list';
+import countryTeplate from './templates/country_entires';
+let debounce = require('lodash.debounce');
+
+const refs = {
+    input: document.querySelector('.inputJS'),
+    target: document.querySelector('.target'),
+};
+let markup = ''
+let searchQuery = ''
+
+refs.input.addEventListener('input', debounce(onTextInput, 500));
+
+function onTextInput(e) {
+    markup = ''
+    searchQuery = e.target.value
+    fetch(`https://restcountries.eu/rest/v2/name/${searchQuery}`).then(r => r.json()).then((data) => {
+    console.log(data.length);
+
+    
+    if (data.length === 1) {
+        markup = countryTeplate(data);       
+    }
+
+    else if (data.length > 1 && data.length <= 10) {
+        markup = listTemplate(data)
+    } 
+    else {
+    console.log('alert');
+    }
+    
+    refs.target.innerHTML = markup;
+})
+    
+}
+
+
+
+
+
